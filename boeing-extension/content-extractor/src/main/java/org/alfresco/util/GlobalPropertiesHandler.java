cbc17fb434710a8bc545a6636118764b31276b02
Sherry Mathews <sherry.mathews@hyland.com>
2024-08-29 18:56:37 -0400

    Create GlobalPropertiesHandler.java

---
+++ boeing-extension/content-extractor/src/main/java/org/alfresco/util/GlobalPropertiesHandler.java
@@ -0,0 +1,17 @@
+package org.alfresco.util;
+
+public class GlobalPropertiesHandler {
+
+    private String customerName;
+
+    public void setCustomerName(String name) {
+        this.customerName = name;
+    }
+
+    public String getCustomerName() {
+        return this.customerName;
+    }
+
+
+
+}
