f6b9721bc73bf060afa11e8c5036e9266f0c0e8f
Sherry Mathews <sherry.mathews@hyland.com>
2024-08-30 17:18:17 -0400

    Update GlobalPropertiesHandler.java

--- boeing-extension/content-extractor/src/main/java/org/alfresco/util/GlobalPropertiesHandler.java
+++ boeing-extension/content-extractor/src/main/java/org/alfresco/util/GlobalPropertiesHandler.java
@@ -1,17 +1,61 @@
 package org.alfresco.util;
 
+import org.alfresco.model.BoeingContentModel;
+import org.alfresco.model.BoeingContentStyleModel;
+
 public class GlobalPropertiesHandler {
 
-    private String customerName;
+    private String customerName = "";
+    public BoeingContentModel boeingContentModel = new BoeingContentModel();
+    public BoeingContentStyleModel boeingContentStyleModel = new BoeingContentStyleModel();
 
-    public void setCustomerName(String name) {
-        this.customerName = name;
-    }
 
     public String getCustomerName() {
         return this.customerName;
     }
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+    public void setBoeingNamespace(String boeingNamespace) {
+        this.boeingContentModel.boeing_namespace = boeingNamespace;
+    }
+    public void setBoeingAspectName(String boeingAspectName) {
+        this.boeingContentModel.boeing_aspectName = boeingAspectName;
+    }
+    public void setWritingSetProperty(String writingSetProperty) {
+        this.boeingContentModel.writingSetProperty = writingSetProperty;
+    }
+    public void setReferencesListProperty(String referencesListProperty) {
+        this.boeingContentModel.referencesListProperty = referencesListProperty;
+    }
+    public void setAuthorityReferencesProperty(String authorityReferencesProperty) {
+        this.boeingContentModel.authorityReferencesProperty = authorityReferencesProperty;
+    }
+    public void setAuthorityReferencesListProperty(String authorityReferencesListProperty) {
+        this.boeingContentModel.authorityReferencesListProperty = authorityReferencesListProperty;
+    }
+    public void setAuthorityReferencesSectionTitle(String authorityReferencesSectionTitle) {
+        this.boeingContentModel.AUTHORITY_REFERENCE_TITLE = authorityReferencesSectionTitle;
+    }
 
+    /////////////////////////////////////////
 
-
+    public void setSectionFontName(String fontName) {
+        this.boeingContentStyleModel.SECTION_FONT_NAME = fontName;
+    }
+    public void setSectionFontSize(Double fontSize) {
+        this.boeingContentStyleModel.SECTION_FONT_SIZE = fontSize;
+    }
+    public void setSectionFontBold(Boolean isBold) {
+        this.boeingContentStyleModel.SECTION_FONT_BOLD = isBold;
+    }
+    public void setTitleFontName(String fontName) {
+        this.boeingContentStyleModel.TITLE_FONT_NAME = fontName;
+    }
+    public void setTitleFontSize(Double fontSize) {
+        this.boeingContentStyleModel.TITLE_FONT_SIZE = fontSize;
+    }
+    public void setTitleFontBold(Boolean isBold) {
+        this.boeingContentStyleModel.TITLE_FONT_BOLD = isBold;
+    }
 }
